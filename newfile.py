966da2b74fce3efe3967110b980a105d6e753ff6
srividhya2108 <anithaannamalai2108@gmail.com>
2025-07-04 14:58:23 +0530

    Add files via upload
    Added calculator. Py

---
+++ newfile.py
@@ -0,0 +1,9 @@
+name=(input("Hi there! Whats ur name?"))
+dob=int(input("Date of birth:"))
+if(dob==18/06):
+	print("Happy Birthday")
+else:
+	print ("Belated or Advance wishes")
+print ("Wishes by sri vidhya")
+
+
